2023ecd252d99d97f51cce52e5928d7bf55f5dd6
Aron <aron.0504@hotmail.com>
2022-09-06 20:56:23 -0300

    controller search with index action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,10 @@
 class SearchController < ApplicationController
   def index
+    @movies = []
+
+    unless params[:query].blank?
+      search_movies = SearchMovies.new(params[:query])
+      @movies = search_movies.movies
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'search/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'search', to: 'search#index', as: 'search_index'
 end
