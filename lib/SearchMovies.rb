5a0daeb256af458d5352cd0a80075fb9546983d8
Aron <aron.0504@hotmail.com>
2022-09-05 22:56:41 -0300

    create search movies lib

---
+++ lib/SearchMovies.rb
@@ -0,0 +1,51 @@
+require 'open-uri'
+require 'nokogiri'
+
+class SearchMovies
+  attr_reader :movies
+
+  def initialize(title)
+    @movies = []
+
+    load_site(title)
+    load_movies
+  end
+
+  private
+
+  def load_site(title)
+    title = title.gsub(' ', '%20')
+    @document = Nokogiri::HTML(URI.open("https://www.imdb.com/find?q=#{title}&s=tt&ttype=ft"))
+  end
+
+  def load_movies
+    @document.search('table[class="findList"] tr').each do |tr|
+      imdb = imdb_value(tr)
+      title = tr.at('td.result_text a').content
+      url_cover = tr.at('td.primary_photo a img').attr('src')
+      year = year_value(tr)
+
+      @movies.append({
+        imdb: imdb,
+        title: title,
+        url_cover: url_cover,
+        year: year
+      })
+    rescue
+      next
+    end
+  end
+
+  def imdb_value(tr)
+    imdb = tr.at('td.result_text a').attr('href')
+    imdb = imdb.split('/title/')[1]
+    imdb.split('/')[0]
+  end
+
+  def year_value(tr)
+    year = tr.at('td.result_text').content
+    year = year.split('(')[1]
+    year = year.split(')')[0]
+    Integer(year)
+  end
+end
