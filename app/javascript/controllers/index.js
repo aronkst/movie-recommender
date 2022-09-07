78cb52902a549ee2958583dd17cfe385ca00a725
Aron <aron.0504@hotmail.com>
2022-09-07 11:49:32 -0300

    finish loading modal and created reload stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import ReloadController from "./reload_controller"
+application.register("reload", ReloadController)
--- app/javascript/controllers/reload_controller.js
+++ app/javascript/controllers/reload_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    Turbo.visit(location.pathname + location.search);
+    this.element.remove();
   }
 }
