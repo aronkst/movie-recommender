5b5832e79ef4017513d6198fa52f520e62647240
Aron <aron.0504@hotmail.com>
2022-09-06 22:43:26 -0300

    fixes on html

--- app/helpers/state_helper.rb
+++ app/helpers/state_helper.rb
@@ -1,6 +1,6 @@
 module StateHelper
   def button_state(state, value, button)
-    button_to states_upsert_path(state.imdb), method: :post, form_class: "inline", class: "btn #{state.value != value ? "btn-outline " : ""}btn-#{button} mr-2 mt-2", params: { value: value } do
+    button_to states_upsert_path(state.imdb), method: :post, form_class: "inline", class: "btn #{state.value == value ? "" : "btn-outline"} #{button} mr-2 mt-2", params: { value: value } do
       yield
     end
   end
