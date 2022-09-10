59592b165fb79e32fb0afaf69528ef31a6523248
Aron <aron.0504@hotmail.com>
2022-09-09 22:24:01 -0300

    rename state helper

--- app/helpers/states_helper.rb
+++ app/helpers/states_helper.rb
@@ -1,4 +1,4 @@
-module StateHelper
+module StatesHelper
   def button_state(state, value, button)
     button_to states_upsert_path(state.imdb), method: :post, form_class: "inline", class: "btn #{state.value == value ? "" : "btn-outline"} #{button} mr-2 mt-2", params: { value: value } do
       yield
