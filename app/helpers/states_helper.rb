13b97f3eaf27c1d8252c090bc8ecbc2d60b35287
Aron <aron.0504@hotmail.com>
2022-09-10 13:25:33 -0300

    turbo stream check if from search

--- app/helpers/states_helper.rb
+++ app/helpers/states_helper.rb
@@ -1,6 +1,6 @@
 module StatesHelper
-  def button_state(state, value, button)
-    button_to states_upsert_path(state.imdb), method: :post, form_class: "inline", class: "btn #{state.value == value ? "" : "btn-outline"} #{button} mr-2 mt-2", params: { value: value } do
+  def button_state(state, value, button, from_search)
+    button_to states_upsert_path(state.imdb), method: :post, form_class: "inline", class: "btn #{state.value == value ? "" : "btn-outline"} #{button} mr-2 mt-2", params: { value: value, from_search: from_search } do
       yield
     end
   end
--- app/javascript/controllers/reload_controller.js
+++ app/javascript/controllers/reload_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    Turbo.visit(location.pathname + location.search);
+    Turbo.visit(location.pathname + location.search, { action: "replace" });
     this.element.remove();
   }
 }
