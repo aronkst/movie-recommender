dd0416fd6ec63ae16ae823a2f321354b1efba998
Aron <aron.0504@hotmail.com>
2022-09-11 11:08:05 -0300

    fixes tests

--- test/controllers/recommended_movies_controller_test.rb
+++ test/controllers/recommended_movies_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class RecommendedMoviesControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get recommended_movies_index_url
+    get root_url
     assert_response :success
   end
 end
