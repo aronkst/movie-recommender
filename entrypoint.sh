0f88205f70bfe2742e5a6098570e0ec848b23971
Aron <aron.0504@hotmail.com>
2022-09-11 11:50:56 -0300

    created dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+rm -f /usr/src/app/tmp/pids/server.pid
+
+echo "bundle install..."
+bundle check || bundle install --jobs 4
+
+echo "yarn install..."
+yarn install --check-files
+
+echo "rails compile assets..."
+bundle exec rails assets:precompile
+
+exec "$@"
